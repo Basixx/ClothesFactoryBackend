b9e84504b89aad6eaede34bdbdd256344658d971
Barbara Rutkowska <barbara_rutkowska.22@wp.pl>
2022-04-18 13:41:56 +0200

    First setup commit

---
+++ src/main/java/com/kodilla/ClothesFactoryBackend/ClothesFactoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.ClothesFactoryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesFactoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesFactoryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/ClothesFactoryBackend/ClothesFactoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.ClothesFactoryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesFactoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
